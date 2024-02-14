815dd71a0f6426d3598d0d0f4f8d22a17f6d20e6
unknown <holupkern@gmail.com>
2024-02-14 20:53:54 +0800

    completed 1. object property checker 2. mergesort using object property in array of object 3. completed ranking by largest and ranking by smallest

--- statistics-formula.js
+++ statistics-formula.js
@@ -2,7 +2,8 @@
 // 1st parameter expects an array of object
 // 2nd parameter expects a string matching object key in array of object
 
-function mergeSort(arrayOfObjects, propertyToSortBy){
+
+function checkProperty (arrayOfObjects, propertyToSortBy){
 
     let errorCatchingIndex;
     for (let object of arrayOfObjects){
@@ -15,14 +16,20 @@ function mergeSort(arrayOfObjects, propertyToSortBy){
             return;
         }
     }
+    console.log("successfully passed check for existence of property in all objects in array");
+}
+
+function mergeSort(arrayOfObjects, propertyToSortBy){
 
     if(arrayOfObjects.length <= 1){
         return arrayOfObjects;
-    }                              // breaks the recursive eventually
+    }                              // breaks the recursion eventually
 
-    let middleIndex = Math.floor(array.length/2);
-    let left = mergeSort(arrayOfObjects.slice(0,middleIndex));
-    let right = mergeSort(arrayOfObjects.slice(middleIndex));
+    let targetProperty = propertyToSortBy;
+
+    let middleIndex = Math.floor(arrayOfObjects.length / 2);
+    let left = mergeSort(arrayOfObjects.slice(0,middleIndex), targetProperty);
+    let right = mergeSort(arrayOfObjects.slice(middleIndex), targetProperty);
 
     let sortedValues = [];
 
@@ -30,14 +37,92 @@ function mergeSort(arrayOfObjects, propertyToSortBy){
     let rightIndexSlider = 0;
 
     while (leftIndexSlider < left.length && rightIndexSlider < right.length){
-        if(left[leftIndexSlider].propertyToSortBy < right[rightIndexSlider].propertyToSortBy){
+
+        if(left[leftIndexSlider][targetProperty] < right[rightIndexSlider][targetProperty]){
+            // console.log(`left value here ${left[leftIndexSlider][targetProperty]} vs right value here ${right[rightIndexSlider][targetProperty]}`)
             sortedValues.push(left[leftIndexSlider]);
             leftIndexSlider ++;
+        } else {
+            // console.log(`left value here ${left[leftIndexSlider][targetProperty]} vs right value here ${right[rightIndexSlider][targetProperty]}`)
+            sortedValues.push(right[rightIndexSlider]);
+            rightIndexSlider ++;
         }
-        
-
-
-
     }
 
+    sortedValues.push(...left.slice(leftIndexSlider));
+    sortedValues.push(...right.slice(rightIndexSlider)); //push remainder item not pushed after comparison
+
+    return sortedValues;
 }
+
+let test1 = [
+                {"id":1, "meow3": 3997, "meow1": 500, "meow2": 1000},
+                {"id":2, "meow3": 4001, "meow1": 500, "meow2": 1000},
+                {"id":3, "meow3": 3998, "meow1": 500, "meow2": 1000},
+                {"id":4, "meow3": 3996, "meow1": 500, "meow2": 1000},
+                {"id":5, "meow3": 4000, "meow1": 500, "meow2": 1000},           
+                {"id":6, "meow3": 3999, "meow1": 500, "meow2": 1000},
+            ]
+
+// let test2 = [
+//     {"meow3": 3997, "meow1": 500, "meow2": 1000},
+//     {"meow3": 4001, "meow1": 500, "meow2": 1000},
+//     {"meow3": 3998, "meow1": 500, "meow2": 1000},
+//     {"meow3": 3996, "meow1": 500, "meow2": 1000},
+//     {"meow3": 4000, "meow1": 500, "meow2": 1000},           
+//     {"meow3": 3999, "meow1": 500},
+// ]
+
+// checkProperty(test2, "meow1");
+// checkProperty(test1, "meow3");
+
+let sortedArray = mergeSort(test1, "meow3")
+
+function rankByPropertySmallestFirst(sortedArrayOfObjects, propertyToRankBy, idOfObjectRecevingRank){
+
+    let targetProperty = propertyToRankBy;
+    let foundIndex;
+
+    for (let object of sortedArrayOfObjects){
+        if (object.id == idOfObjectRecevingRank){
+            foundIndex = sortedArrayOfObjects.indexOf(object);
+        }
+    }
+
+    let rank;
+    if (foundIndex){
+        rank = foundIndex + 1;
+        console.log(`Rank of object with id${idOfObjectRecevingRank} is rank ${rank} smallest out of ${sortedArrayOfObjects.length} peers based on criteria of ${targetProperty}`);
+    } else {
+        console.log("Id of object is not found");
+        return;
+    }
+}
+
+// rankByPropertySmallestFirst(sortedArray, "meow3", 2);
+
+function rankByPropertyBiggestFirst(sortedArrayOfObjects, propertyToRankBy, idOfObjectRecevingRank){
+
+    let targetProperty = propertyToRankBy;
+    let foundIndex;
+
+    for (let object of sortedArrayOfObjects){
+        if (object.id == idOfObjectRecevingRank){
+            foundIndex = sortedArrayOfObjects.indexOf(object);
+        }
+    }
+
+    let rank;
+    if (foundIndex){
+        let arrayLength = sortedArrayOfObjects.length;
+
+        rank = arrayLength - foundIndex;
+
+        console.log(`Rank of object with id${idOfObjectRecevingRank} is: rank ${rank} largest out of ${sortedArrayOfObjects.length} peers based on criteria of ${targetProperty}`);
+    } else {
+        console.log("Id of object is not found");
+        return;
+    }
+}
+
+rankByPropertyBiggestFirst(sortedArray, "meow3", 2);
