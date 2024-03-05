c04a06df5953ad1a1a4fa5fa7bc8a0f842850edb
unknown <holupkern@gmail.com>
2024-03-05 21:56:03 +0800

    added simple array merge sort, range, range by property, and also work in progress two array box plot statistics generator

--- statistics-formula.js
+++ statistics-formula.js
@@ -51,6 +51,39 @@
         return sortedValues;
     }
 
+    function simpleArrayMergeSort(simpleArray){
+
+        let currentArray = simpleArray;
+        if (currentArray.length <= 1){
+            return currentArray;
+        }
+
+        let middleIndex = Math.floor(currentArray.length / 2); 
+        let left = simpleArrayMergeSort(currentArray.slice(0, middleIndex));
+        let right = simpleArrayMergeSort(currentArray.slice(middleIndex));
+        
+        let sortedValues = [];
+
+        let leftIndexSlider = 0;
+        let rightIndexSlider = 0;
+
+        while (leftIndexSlider < left.length && rightIndexSlider < right.length ){
+            if (left[leftIndexSlider] < right[rightIndexSlider]){
+                sortedValues.push(left[leftIndexSlider]);
+                leftIndexSlider++;
+            } else {
+                sortedValues.push(right[rightIndexSlider]);
+                rightIndexSlider++;
+            }
+        }
+
+        sortedValues.push(...left.slice(leftIndexSlider));
+        sortedValues.push(...right.slice(rightIndexSlider));
+
+        return sortedValues;
+    }
+
+
     function rankByPropertySmallestFirst(sortedArrayOfObjects, propertyToRankBy, idOfObjectRecevingRank){
 
         let targetProperty = propertyToRankBy;
@@ -256,9 +289,117 @@
     }
 
 
+
+
+    function rangeOfArrayOfObjects(arrayOfObjects, property){
+
+        let sortedArray = mergeSortByProperty(arrayOfObjects, property);
+        const largestValue = sortedArray[sortedArray.length-1][property];
+        const smallestValue = sortedArray[0][property];
+        let range = largestValue - smallestValue;
+        console.log(range);
+        return range;
+    }
+
+    function simpleRange(unSortedArray){
+        let sortedArray = simpleArrayMergeSort(unSortedArray);
+        const largestValue = sortedArray[sortedArray.length -1];
+        const smallestValue = sortedArray[0];
+        let range = largestValue - smallestValue;
+        console.log(range);
+        return range;
+    }
+
+    // function dissectingAFunction(unsortedArray1, unsortedArray2){
+    //     console.log("arguments.callee", arguments.callee);
+    //     console.log("arguments.callee.name", arguments.callee.name);
+    //     console.log("arguments.callee.arguments", arguments.callee.arguments);
+    // }
+
+
+    // Need to test and also refactor ---> got alot of wasted codes, need to create output
+    function simpleTwoNominalCategoryBoxPlot(unsortedArray1, unsortedArray2){
+
+        let array1 = unsortedArray1;
+        let array2 = unsortedArray2;
+
+        let sortedArray1 = simpleArrayMergeSort(array1);
+        let sortedArray2 = simpleArrayMergeSort(array2);
+
+        let arrayLength1 = sortedArray1.length;
+        let arrayLength2 = sortedArray2.length;
+
+        let indexOf25PercentileForFirst = Math.ceil((25 / 100) * (arrayLength1 - 1));
+        let indexOf50PercentileForFirst = Math.ceil((50 / 100) * (arrayLength1 - 1));
+        let indexOf75PercentileForFirst = Math.ceil((75 / 100) * (arrayLength1 - 1));
+
+        let median1 = median(sortedArray1);
+        let median2 = median(sortedArray2);
+        
+        // for first array 
+        let dataSet1 = {};
+        dataSet1[upperHinge] = sortedArray1[indexOf75PercentileForFirst];
+        dataSet1[lowerHinge] = sortedArray1[indexOf25PercentileForFirst];
+        dataSet1[hspread] = dataSet1[upperHinge1] - dataSet1[lowerHinge1];
+        dataSet1[step] = 1.5 * dataSet1[hspread];
+        dataSet1[upperInnerFence] = dataSet1[upperHinge] + dataSet1[step];
+        dataSet1[upperOuterFence] = dataSet1[upperHinge] + (dataSet1[step] * 2)
+        dataSet1[lowerInnerFence] = dataSet1[lowerHinge] - (dataSet1[step])
+        dataSet1[lowerOuterFence] = dataSet1[lowerHinge] - (dataSet1[step] * 2)
+        
+        let processedDataHolder = [];
+        let valueFinder = (sortedArray, dataSet) => {
+            let arrayToModify = sortedArray;
+            arrayToModify.push(dataSet[upperInnerFence]);
+            arrayToModify.push(dataSet[lowerInnerFence]);
+            arrayToModify.push(dataSet[upperOuterFence]);
+            arrayToModify.push(dataSet[lowerOuterFence]);
+
+            let sortedModifiedArray = simpleArrayMergeSort(arrayToModify);
+            let indexOfUpperAdj = sortedModifiedArray.indexOf(dataSet[upperInnerFence]) - 1;
+            let indexOfLowerAdj = sortedModifiedArray.indexOf(dataSet[lowerInnerFence]) + 1;
+
+            let indexOfUpperOuterFence = sortedModifiedArray.indexOf(dataSet[upperOuterFence]);
+            let indexOfLowerOuterFence = sortedModifiedArray.indexOf(dataSet[lowerOuterFence]);
+            
+            let outsideUpperValues = sortedModifiedArray.slice(indexOfUpperAdj + 2, indexOfUpperOuterFence);
+            let outsideLowerValues = sortedModifiedArray.slice(indexOfLowerOuterFence, indexOfLowerAdj - 1);
+
+            let farOutUpperValues = null;
+            if (indexOfUpperOuterFence !== sortedModifiedArray.length - 1){
+                farOutUpperValues = sortedModifiedArray.slice(indexOfUpperOuterFence + 1)
+            }
+
+            let farOutLowerValues = null;
+            if (indexOfLowerOuterFence !== 0){
+                farOutLowerValues = sortedModifiedArray.slice(0, indexOfLowerOuterFence)
+            }
+
+            dataSet[upperAdj] = sortedModifiedArray[indexOfUpperAdj];
+            dataSet[lowerAdj] = sortedModifiedArray[indexOfLowerAdj];
+            dataSet[outsideUpperValues] = sortedModifiedArray[outsideUpperValues];
+            dataSet[outsideLowerValues] = sortedModifiedArray[outsideLowerValues];
+            dataSet[farOutUpperValues] = farOutUpperValues;
+            dataSet[farOutLowerValues] = farOutLowerValues;
+
+            processedDataHolder.push(dataSet);
+        }
+
+        
+
+        // for second array
+
+
+
+    }
+
+
+
+
 module.exports={
     checkProperty,
     mergeSortByProperty,
+    simpleArrayMergeSort,
     rankByPropertySmallestFirst,
     rankByPropertyBiggestFirst,
     retrieveDataByPercentile,
@@ -267,7 +408,24 @@ module.exports={
     mean,
     median,
     mode,
-    standardDeviationPopulation
+    standardDeviationPopulation,
+    rangeOfArrayOfObjects,
+    simpleRange,
+    
 }
 
+const sampleArray = [
+    { "first": 10, "second": 20, "third": 30, "fourth": 40 },
+    { "first": 15, "second": 25, "third": 35, "fourth": 45 },
+    { "first": 12, "second": 22, "third": 32, "fourth": 42 },
+    { "first": 18, "second": 28, "third": 38, "fourth": 48 }
+    ];
 
+    const simpleSampleArray = [
+        47, 82, 15, 33, 69,
+        28, 91, 56, 10, 72,
+        39, 23, 58, 41, 79,
+        64, 12, 53, 86, 97
+    ];
+
+    dissectingAFunction(sampleArray, simpleSampleArray);
